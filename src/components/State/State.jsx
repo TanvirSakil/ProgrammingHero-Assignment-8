5d03c6bd874c348cedd63c74c6ab7d935516e2d3
TanvirSakil <tanvirbt14@gmail.com>
2025-10-19 19:47:49 +0600

    Adding state section

---
+++ src/components/State/State.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const State = () => {
+    return (
+        
+            <div className='bg-gradient-to-b from-cyan-700 via-blue-500 to-indigo-600 text-white p-10 '>
+<h3 className='text-3xl font-bold text-center '>Trusted By Millions, Built For You</h3>
+
+<div className='flex justify-evenly mt-6  '>
+
+<div className='text-center'>
+    <h3 className=' mb-2 text-gray-200'>Total Downloads</h3>
+    <h3 className='text-5xl font-bold '>29.6M</h3>
+    <h3 className='mt-4 text-gray-200'>21% more Than Last Month</h3>
+</div>
+<div className='text-center'>
+    <h3 className=' mb-2 text-gray-200'>Total Review</h3>
+    <h3 className='text-5xl font-bold '>906K</h3>
+    <h3 className='mt-4 text-gray-200'>46% more Than Last Month</h3>
+</div>
+<div className='text-center'>
+    <h3 className=' mb-2 text-gray-200'>Active Apps</h3>
+    <h3 className='text-5xl font-bold '>132+</h3>
+    <h3 className='mt-4 text-gray-200'>31 more Will Launch</h3>
+</div>
+
+</div>
+
+        </div>
+       
+    );
+};
+
+export default State;
